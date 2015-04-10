49e00e23bb10ed0f220de349824565a5b2fb3b70
g12948ss <g12948ss@s30454.tsuda.ac.jp>
2015-04-10 11:07:52 +0900

    表示内容を変更しました。

---
+++ src/helloworld1/HelloWorld1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package helloworld1;
+
+/**
+ *
+ * @author g12948ss
+ */
+public class HelloWorld1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         System.out.println("Hello Hello");
+        System.out.println("Good Bye");
+        // TODO code application logic here
+    }
+}
